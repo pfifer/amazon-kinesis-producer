179fb2aa15027a3c5466ff1b7d354d320e0d7db3
Pfifer, Justin <pfifer@amazon.com>
2017-03-03 09:28:51 -0800

    Made the test more complex, and include actual handlers in the chain to see how unwind affects stack trace.
    Unfortunately for clang it appears that the stack trace is post unwind, still need to check for linux.

--- aws/utils/test/terminate_handler_test.cpp
+++ aws/utils/test/terminate_handler_test.cpp
@@ -7,15 +7,71 @@
 #include <system_error>
 #include <exception>
 #include <iostream>
+#include <sstream>
 
 void verify_terminate_handler() {
-  std::cout << "Chained termination handler was called as expected" << std::endl;
+  std::cout << "Chained termination handler was called as expected" << std::endl << std::flush;
 }
+
+class Test3 {
+public:
+  void level_3(int how) {
+    if (how > 100) {
+      throw 10;
+    }
+    if (how > 1000) {
+      std::stringstream ss;
+      ss << "How is greater than 1000: " << how;
+      throw std::runtime_error(ss.str());
+    }
+    if (how > 9000) {
+      std::stringstream ss;
+      ss << "What does the scouter say? It's over 9000!!: " << how;
+      throw std::system_error(std::make_error_code(std::errc::no_buffer_space));
+    }
+    std::cout << "How is below 100 no exception" << std::endl << std::flush;
+  }
+};
+
+class Test2 {
+public:
+  Test3 test3;
+  void level_2(int how) {
+    try {
+      test3.level_3(how);
+    } catch (int code) {
+      std::cout << "Caught code: " << code << std::endl << std::flush;
+      std::stringstream message;
+      message << "Caught code: " << code;
+      throw std::runtime_error(message.str());
+    }
+  }
+};
+
+class Test1 {
+public:
+  Test2 test2;
+  void level_1(int how) {
+    try {
+      test2.level_2(how);
+    } catch (std::runtime_error& err) {
+      std::cout << "Caught runtime_error: " << err.what() << std::endl << std::flush;
+    }
+  }
+};
+
+
 
 int main(int argc, char** argv) {
   std::set_terminate(&verify_terminate_handler);
   aws::utils::setup_stack_trace(argv[0]);
-  std::cout << "Preparing to throw an unhandled exception." << std::endl;
+  Test1 test;
+
+  test.level_1(10);
+  test.level_1(101);
+  test.level_1(1001);
+  std::cout << "Now ready to cause unhandled exception" << std::endl << std::flush;
+  test.level_1(9001);
 
   throw std::system_error(std::make_error_code(std::errc::invalid_argument));
 
