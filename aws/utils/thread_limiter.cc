c20501b1071fb162e46e469f6300760db5cf5b0f
Pfifer, Justin <pfifer@amazon.com>
2017-04-27 08:09:32 -0700

    Updater thread that handles changes to a dynamic config. Allowing easier testing of thread limits.

--- aws/utils/thread_limiter.cc
+++ aws/utils/thread_limiter.cc
@@ -8,19 +8,22 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-#include "logging.h"
+#include <thread>
+#include <memory>
+#include <chrono>
 #include <cstdint>
-
+#include <exception>
+#include <system_error>
 #include <sys/resource.h>
+#include <sys/stat.h>
+
+#include "logging.h"
 
 #include "thread_limiter.h"
 
 using namespace aws::utils;
 
 namespace {
-
-
-
 
   uint32_t config_from_env() {
     const char *env_control = std::getenv("KPL_THREAD_POOL");
@@ -34,8 +37,7 @@ namespace {
     return thread_count;
   }
 
-  uint32_t config_from_file() {
-    std::string config_file("/app/tester/conf/thread_limit.cfg");
+  uint32_t config_from_file(const std::string &config_file) {
     std::ifstream input_file(config_file);
     if (input_file.bad()) {
       LOG(warning) << "Failed to open '" << config_file << "' for reading.";
@@ -55,34 +57,116 @@ namespace {
 
     return thread_limit;
   }
+
+  void update_thread_limit(const std::string &config_file) {
+    /*
+ * This is a hack to quickly test the two different thread pools using the same code base.  It hinges off
+ * a file in a known directory
+ */
+
+    uint32_t thread_count = config_from_env();
+    if (thread_count == 0) {
+      thread_count = config_from_file(config_file);
+    }
+
+    if (thread_count != 0) {
+      rlimit rlim = {0};
+      if (getrlimit(RLIMIT_NPROC, &rlim)) {
+        LOG(error) << "Failed to retrieve NPROC rlimit";
+        return;
+      }
+      auto old = rlim.rlim_cur;
+      rlim.rlim_cur = thread_count;
+
+
+      if (setrlimit(RLIMIT_NPROC, &rlim)) {
+        LOG(error) << "Failed to update rlimit: " << old << " to " << rlim.rlim_cur;
+      } else {
+        LOG(info) << "Set thread limit from " << old << " to " << rlim.rlim_cur;
+      }
+
+    }
+  }
+
+  class thread_limiter {
+    using clock = std::chrono::system_clock;
+    using time_point = std::chrono::time_point<clock>;
+
+    std::unique_ptr<std::thread> update_thread_;
+    time_point last_dynamic_config_update_;
+    std::string static_config_file_;
+    std::string dynamic_config_file_;
+
+    std::chrono::milliseconds update_check_;
+
+
+
+    time_point last_dynamic_update() {
+      struct stat dynamic_check;
+      time_t last_update;
+      if (stat(dynamic_config_file_.c_str(), &dynamic_check) == -1) {
+        if (errno != ENOENT) {
+          throw std::system_error(errno, std::generic_category());
+        }
+        last_update = 0;
+      } else {
+        last_update = dynamic_check.st_mtimespec.tv_sec;
+      }
+      return clock::from_time_t(last_update);
+    }
+
+    std::string display_time_point(const time_point& tp) {
+      struct tm lt;
+      std::time_t tt = clock::to_time_t(tp);
+      localtime_r(&tt, &lt);
+
+      char display[255] = { 0 };
+
+      strftime(display, sizeof(display) - 1, "%FT%T", &lt);
+
+      std::string result(display);
+      return result;
+    }
+
+    void check_update() {
+      while (true) {
+        std::this_thread::sleep_for(update_check_);
+
+        LOG(info) << "Checking for changes to " << dynamic_config_file_;
+        time_point last_updated = last_dynamic_update();
+        if (last_updated > last_dynamic_config_update_) {
+          LOG(info) << "Old Version: " << display_time_point(last_dynamic_config_update_)
+                    << " -- New Version: " << display_time_point(last_updated);
+          last_dynamic_config_update_ = last_updated;
+          update_thread_limit(dynamic_config_file_);
+        } else {
+          LOG(info) << "No changes to " << dynamic_config_file_;
+        }
+      }
+    }
+
+  public:
+
+    thread_limiter() : static_config_file_("/app/tester/conf/thread_limit.cfg"),
+                       dynamic_config_file_("/tmp/thread_limit.cfg") {
+      {
+        using namespace std::chrono_literals;
+        update_check_ = std::chrono::duration_cast<std::chrono::milliseconds>(10s);
+      }
+
+      last_dynamic_config_update_ = last_dynamic_update();
+      update_thread_ = std::make_unique<std::thread>(&thread_limiter::check_update, this);
+      update_thread_->detach();
+      update_thread_limit(static_config_file_);
+    }
+
+  };
+
+  std::shared_ptr<thread_limiter> g_thread_limiter;
 }
 
 void aws::utils::set_thread_limit() {
-  /*
-   * This is a hack to quickly test the two different thread pools using the same code base.  It hinges off
-   * a file in a known directory
-   */
-
-  uint32_t thread_count = config_from_env();
-  if (thread_count == 0) {
-    thread_count = config_from_file();
-  }
-
-  if (thread_count != 0) {
-    rlimit rlim = { 0 };
-    if (getrlimit(RLIMIT_NPROC, &rlim)) {
-      LOG(error) << "Failed to retrieve NPROC rlimit";
-      return;
-    }
-    auto old = rlim.rlim_cur;
-    rlim.rlim_cur = thread_count;
-
-
-    if (setrlimit(RLIMIT_NPROC, &rlim)) {
-      LOG(error) << "Failed to update rlimit: " << old << " to " << rlim.rlim_cur;
-    } else {
-      LOG(info) << "Set thread limit from " << old << " to " << rlim.rlim_cur;
-    }
-
+  if (g_thread_limiter == nullptr) {
+    g_thread_limiter = std::make_shared<thread_limiter>();
   }
 }
